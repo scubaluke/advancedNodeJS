4eebea75ce2d1969cbc06b57e94fb7b60714a283
Luke Gartland <lukegartland@gmail.com>
2021-07-06 17:16:23 -0500

    pm2 testing

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
+process.env.UV_THREADPOOL_SIZE = 1
+
 const cluster = require('cluster');
-
+const crypto = require('crypto');
 // is the file being executed in master mode? 
-if (cluster.isMaster) {
-    // cause index.js to be executed again in child mode
-    cluster.fork()
-    cluster.fork()
-    cluster.fork()
-    cluster.fork()
 
-} else {
-    // child, act like a server and nothing else
-    const express  = require('express')
-    const  app = express() 
-    function doWork(duration) {
-        const start = Date.now()
-        while (Date.now() - start < duration) {}
-    }
+// child, act like a server and nothing else
+const express  = require('express')
+const  app = express() 
 
-    app.get('/', (req, res) => {
-        doWork(5000)
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () =>  {
         res.send('HI there')
-    })
-    app.get('/fast', (req, res) =>  {
-        res.send('this was fast')
-    })
 
-    app.listen(3000)
-}
+        })
+})
+app.get('/fast', (req, res) =>  {
+    res.send('this was fast')
+})
+
+app.listen(3000)
