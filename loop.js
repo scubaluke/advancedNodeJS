91919daf3151964b878ee890d4c013199da42c48
Luke Gartland <lukegartland@gmail.com>
2021-07-06 10:22:55 -0500

    threadpools and changing threadpool size

--- loop.js
+++ loop.js
@@ -16,7 +16,15 @@ function shouldContinue() {
 
 //  entire body  executes in one 'tick'
 while (shouldContinue) {
-    
+    // 1) node looks at pending timers and sees if any functions are ready to be called. set timout, setInterval
+    // 2) Node  looks at pending OS tasks and pendingOperations and calls relevant callbacks
+    // 3) Pause execution. Continue when... 
+    // - new pending OS task is done
+    // - new pending operation is done
+    // - a timer is about to complete
+
+    //4) look at pending timers. Call any setImmediate
+    //5) handle any 'close' events
 }
 
 
