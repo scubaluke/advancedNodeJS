7eda32c410db8f8aeff537b267397f6314aefdf5
Luke Gartland <lukegartland@gmail.com>
2021-07-09 10:06:03 -0500

    hydrating models

--- AdvancedNodeProjectOne/services/cache.js
+++ AdvancedNodeProjectOne/services/cache.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-    console.log('IM ABOUT TO RUN A QUERY');
-    console.log(this.getQuery());
-    console.log(this.mongooseCollection.name);
+mongoose.Query.prototype.exec = async function () {
+  const  key =  JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name,
+    }))
 
-        return  exec.apply(this,  arguments)
+    // see if we  have value for key  in redis
+    const cacheValue  = await client.get(key)
+    //if we do return that
+    if  (cacheValue) {
+        const doc = new this.model(JSON.parse(cacheValue))
+
+        return doc
+    }
+    //otherwise,  issue the query and store the result in redis. 
+    const result = await exec.apply(this,  arguments)
+
+    client.set(key, JSON.stringify(result))
+    return result
 }
