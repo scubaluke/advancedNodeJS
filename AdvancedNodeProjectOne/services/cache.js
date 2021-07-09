37098cf144f6924842369d654ee95fbcbea6ef79
Luke Gartland <lukegartland@gmail.com>
2021-07-09 09:22:51 -0500

    editing mongoose.query to add caching logic

---
+++ AdvancedNodeProjectOne/services/cache.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+    console.log('IM ABOUT TO RUN A QUERY');
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
+
+        return  exec.apply(this,  arguments)
+}
