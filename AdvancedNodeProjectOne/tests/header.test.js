3d5de871b7455595f9087a278624841b09d39250
Luke Gartland <lukegartland@gmail.com>
2021-07-10 17:29:16 -0500

    custom page reuseable functions

--- AdvancedNodeProjectOne/tests/header.test.js
+++ AdvancedNodeProjectOne/tests/header.test.js
@@ -12,8 +12,8 @@ afterEach( async () => {
 })
 
 test('the header has correct text', async () => {
- 
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+    const text = await page.getContentsOf('a.brand-logo')
+
     expect(text).toEqual('Blogster')
 })
 
@@ -25,7 +25,7 @@ test('clicking login starts Oauth flow', async ()  => {
 
 test('When signed in shows logout button', async () => {
     await page.login()
-    
-    const text = await page.$eval('a[href="/auth/logout"]', el  => el.innerHTML) 
+
+    const text = await page.getContentsOf('a[href="/auth/logout"]') 
     expect(text).toEqual('Logout')
 })
