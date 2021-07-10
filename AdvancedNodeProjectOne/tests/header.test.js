5421b6e3036194728f1ac435c8dd4820feaf641b
Luke Gartland <lukegartland@gmail.com>
2021-07-09 23:00:27 -0500

    session signitures

--- AdvancedNodeProjectOne/tests/header.test.js
+++ AdvancedNodeProjectOne/tests/header.test.js
@@ -1,5 +1,49 @@
-test('adds two numbers', () => {
-    const sum = 1 + 2
+const puppeteer = require('puppeteer');
 
-    expect(sum).toEqual(3)
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    })
+    page = await browser.newPage()
+    await  page.goto('localhost:3000')
+})
+
+// afterEach( async () => {
+//     await browser.close()
+// })
+
+test('the header has correct text', async () => {
+ 
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML)
+    expect(text).toEqual('Blogster')
+})
+
+test('clicking login starts Oauth flow', async ()  => {
+    await page.click('.right a')
+    const url = await page.url()
+    expect(url).toMatch(/accounts\.google\.com/)
+})
+
+test.only('When signed in shows logout button', async () => {
+    const id = '60e745591f688ba88c1f78f2'
+    const buffer = require('safe-buffer').Buffer
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    }
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64')
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys')
+    const  keygrip  = new Keygrip([keys.cookieKey])
+    const sig = keygrip.sign('session='+sessionString)
+
+    await page.setCookie({ name: 'session', value:  sessionString })
+    await page.setCookie({ name: 'session.sig', value: sig })
+    await page.goto('localhost:3000')
+
+
+
 })
