9e7b6a304b46900624f4d98182515820a51a4315
Luke Gartland <lukegartland@gmail.com>
2021-07-11 06:34:27 -0500

    creating blog test

---
+++ AdvancedNodeProjectOne/tests/blogs.test.js
@@ -0,0 +1,16 @@
+const Page = require('./helpers/page');
+
+let page
+
+beforeEach(async () => {
+    page = await page.build()
+    await page.goto('localhost:3000')
+})
+
+afterEach(async () => {
+    await page.close()
+})
+
+test('when logged in, can see blog create form', async () => {
+    await page.login()
+})
