5eeef466cf862cba1a59793b5d7940d5d27d8830
Luke Gartland <lukegartland@gmail.com>
2021-07-13 16:42:39 -0500

    blog test success

--- AdvancedNodeProjectOne/tests/blogs.test.js
+++ AdvancedNodeProjectOne/tests/blogs.test.js
@@ -3,7 +3,7 @@ const Page = require('./helpers/page');
 let page
 
 beforeEach(async () => {
-    page = await page.build()
+    page = await Page.build()
     await page.goto('localhost:3000')
 })
 
@@ -13,4 +13,8 @@ afterEach(async () => {
 
 test('when logged in, can see blog create form', async () => {
     await page.login()
+    await page.click('a.btn-floating')
+
+    const label = await page.getContentsOf('form label')
+    expect(label).toEqual('Blog Title')
 })
