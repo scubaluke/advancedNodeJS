4340896e502bc27693ff00297374931e288f5eb6
Luke Gartland <lukegartland@gmail.com>
2021-07-13 21:45:28 -0500

    adding new blog post test

--- AdvancedNodeProjectOne/tests/blogs.test.js
+++ AdvancedNodeProjectOne/tests/blogs.test.js
@@ -22,6 +22,30 @@ describe('When logged in', async  () => {
         const label = await page.getContentsOf('form label')
         expect(label).toEqual('Blog Title')
     })
+    describe('Aan using valid inputs', async () => {
+        beforeEach(async () => {
+            await page.type('.title input', 'My Title')
+            await page.type('.content input', 'My Content')
+            await page.click('form button')
+
+        })
+        test('Submitting takes user to review screen', async () => {
+            const text = await page.getContentsOf('h5')
+            expect(text).toEqual('Please confirm your entries')
+        })
+        test('Submitting then saving adds blog to index page', async () => {
+            await page.click('button.green')
+            await page.waitFor('.card')
+
+            const title = page.getContentsOf('.card-title')
+            const contents = page.getContentsOf('p')
+
+            expect(title).toEqual('My Title')
+            expect(contents).toEqual('My Content')
+
+        })
+    })
+
     describe('And using invalid inputs', async () => {
         beforeEach(async () => {
             await page.click('form button')
